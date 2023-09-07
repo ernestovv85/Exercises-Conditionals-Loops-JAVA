15d4509f03e41985c6b13e6618ff28f6e86b0876
ernestovv85 <ernestovv85@gmail.com>
2023-09-07 13:13:27 -0600

    Solved exercise 1

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Exercises {
+  private static final Scanner in = new Scanner(System.in);
   //1. Mostrar en consola el mensaje de "Aprobado" en caso la calificación de un alumno sea mayor igual a 70.
   //En caso contrario mostrar el mensaje "Reprobado".
+  public static void excercise1() {
+    System.out.println("La calificación debe ser ingresada en decenas.\n" +
+            "Por ejemplo: 50, 70, 85, 94, etc.");
+    System.out.println("Digite la calificación:");
+    int rating = in.nextInt();
+    if (rating >= 70) {
+      System.out.println("Aprobado");
+    } else {
+      System.out.println("Reprobado");
+    }
+  }
 
   //2. Dado 3 números enteros, mostrar en consola los números de forma ascendente, de menor a mayor.
   //Ejemplo Números = 9, 50, 4 Salida = 4, 9, 50
