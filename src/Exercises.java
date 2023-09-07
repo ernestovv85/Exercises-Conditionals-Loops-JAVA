72c6de0b2e7fcc06700335977ae286f6884c580a
ernestovv85 <ernestovv85@gmail.com>
2023-09-07 12:58:23 -0600

    First commit

---
+++ src/Exercises.java
@@ -0,0 +1,35 @@
+public class Exercises {
+  //1. Mostrar en consola el mensaje de "Aprobado" en caso la calificación de un alumno sea mayor igual a 70.
+  //En caso contrario mostrar el mensaje "Reprobado".
+
+  //2. Dado 3 números enteros, mostrar en consola los números de forma ascendente, de menor a mayor.
+  //Ejemplo Números = 9, 50, 4 Salida = 4, 9, 50
+
+  //3. Imprimir en consola la tabla de multiplicar del número 9.
+
+  //4. Replicar la siguiente salida en consola.
+  //*
+  //**
+  //***
+  //****
+  //*****
+  //******
+
+  //5. Replicar la siguiente salida en consola.
+  //     *
+  //    ***
+  //   *****
+  //  *******
+  // *********
+  //Ayuda: Podrás apoyarte de ciclos anidados así como de los métodos println y print
+
+  //6. Imprimir en consola los primero diez números de la serie Fibonacci.
+
+  //7. Dado un número saber si este es primo o no.
+
+  //8. Imprimir en consola todos los números múltiplos de 2. El ciclo finalizará cuando el último múltiplo sea menor a 4570
+
+  //9. Dado un número entero obtener su factorial.
+
+  //10. Crear un programa el cual nos permita conocer el valor de la constante e.
+}
